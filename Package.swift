290cd2368b36da5569e8934ad71cdbd3d4197d2a
Thomas Bibby <thomas.bibby@horizonfintex.com>
2022-03-01 14:27:13 +0000

    v1.3

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 //        .target(name: "HorizonFintexSDK"),
-        .binaryTarget(name: "HorizonFintexSDK", url: "https://horizonfintex.jfrog.io/artifactory/horizonsdk-cocoapods-local/HorizonFintexSDK/1.2/HorizonFintexSDK.zip", checksum: "f59c405e17b85563337c269cf721bd974523236a3b7dc46edd68fd24fc4a277c"),
+        .binaryTarget(name: "HorizonFintexSDK", url: "https://horizonfintex.jfrog.io/artifactory/horizonsdk-cocoapods-local/HorizonFintexSDK/1.3/HorizonFintexSDK.zip", checksum: "d1c37a2202efd35eec78a071c6ebd48246242bd8a94c03184faad05357d365ec"),
     ]
 )
